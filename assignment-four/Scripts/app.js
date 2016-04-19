ba276a72b305ac73099ff5c52fdc013846828347
blanegraff <brittany.lanegraff@mygeorgian.ca>
2016-04-19 10:14:24 -0400

    Added files via upload
    Navigation Partial was added
    Initial banner ad size was created

--- assignment-four/Scripts/app.js
+++ assignment-four/Scripts/app.js
@@ -31,6 +31,7 @@
 
         firstParagraph.innerHTML = "My name is Brittany Lanegraff and I am currently in the Interactive Web Design & Development program at Georgian College. My strength is in design, but I hope to learn and develop my skills in programming. I come from a photography and illustration background which I am sure will be a great asset to assist me in styling and designing websites. I love the process of putting all the elements together that are required for a fully functional and beautiful website, it is very rewarding and enjoyable. I hope to be able to succeed in having a great career in the web design and development field.";
     };
+    
 //     Javascript CSS Styles
 document.getElementById("firstParagraph").style.width = "1000px";
 document.getElementById("firstParagraph").style.textAlign = "center";
